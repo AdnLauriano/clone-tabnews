87480dd2cad1c9dd955b82c5ba50198201554357
Adonai Lauriano <95664522+AdnLauriano@users.noreply.github.com>
2025-05-19 23:57:09 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Alguma coisa alguma coisa teste</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
